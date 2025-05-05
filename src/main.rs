7929f3a74ece153ff7209ead6406723b4ab34d98
nikishin_dmitrii <ndima25444@gmail.com>
2025-05-05 19:38:39 +0300

    feat: Добавил две опции и сделал проверку днс

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
-use reqwest::blocking::get;
+use reqwest::blocking::Client;
 use std::error::Error;
 use std::fs;
-use std::fs::File;
+use std::fs::{File};
 use std::io::{BufRead, BufReader, Write};
+use std::net::IpAddr;
+use clap::Parser;
+use url::Url;
 
-fn parse_html(url: &str) -> Result<String, Box<dyn Error>> {
-    let response = get(url)?.text()?;
-    Ok(response)
+use trust_dns_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    Resolver,
+};
+
+fn get_doh_ips(host : &str) -> Result<Vec<IpAddr>, Box<dyn Error>>{
+    let config = ResolverConfig::cloudflare_https();
+    let resolver = Resolver::new(config, ResolverOpts::default())?;
+    let doh_ips: Vec<_> = resolver.lookup_ip(host)?.iter().collect();
+    if doh_ips.is_empty() {
+        return Err("DoH не вернул ни одного IP".into());
+    }
+    Ok(doh_ips)
+}
+fn parse_html(url: &Url) -> Result<String, Box<dyn Error>> {
+    // резолвим IP через Cloudflare DoH
+    let hostname = url.host_str().ok_or("Не удалось получить host из URL")?;
+    let doh_ips = get_doh_ips(hostname)?;
+    //println!("IP от DoH: {:?}", doh_ips);
+
+    let client = create_doh_client();
+    let response = client.get(url.clone()).send()?;
+    let actual_ip = response.remote_addr().ok_or("Не удалось получить IP")?;
+
+    //println!("Фактический IP соединения: {}", actual_ip.ip());
+
+    if !doh_ips.contains(&actual_ip.ip()) {
+        return Err("⚠️ Подозрение на DNS spoofing! IP не совпадает с DoH".into());
+    }
+    Ok(response.text()?)
 }
 
 fn extract_bridges(text: &str) -> Vec<String> {
@@ -51,11 +81,51 @@ fn update_torrc(path: &str, bridges: &Vec<String>) -> Result<(), Box<dyn Error>>
     Ok(())
 }
 
+fn create_doh_client() -> Client {
+    let client = Client::builder()
+        .timeout(std::time::Duration::from_secs(30))
+        .build()
+        .unwrap();
+
+    client
+}
+
+#[derive(Parser)]
+#[command(name = "torrc-updater")]
+#[command(about = "Обновляет torrc с мостами", version = "1.0")]
+struct Args {
+    #[arg(short, long, default_value = "/etc/tor/torrc")]
+    torrc: String,
+
+    #[arg(short, long)]
+    print: bool,
+
+    #[arg(short, long)]
+    dry_run: bool,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let html = parse_html("https://torscan-ru.ntc.party/relays.txt")?;
+    let args = Args::parse();
+
+    let url = Url::parse("https://torscan-ru.ntc.party/relays.txt")?;
+    let html = parse_html(&url)?;
     let bridges = extract_bridges(&html);
 
-    update_torrc("torrc", &bridges)?;
+    if args.print || args.dry_run {
+        println!("мосты:");
+        for i in &bridges {
+            println!("{i}");
+        }
+    }
 
+    if args.dry_run {
+        println!("Мосты не были записаны в файл, так как --dry-run активирован");
+        return Ok(());
+    }
+
+    update_torrc(&args.torrc, &bridges)?;
+
+
+    println!("Мосты обновлены");
     Ok(())
 }
