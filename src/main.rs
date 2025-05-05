82813c2afec50d18144d6e0fd32c3c172d8a9f62
nikishin_dmitrii <ndima25444@gmail.com>
2025-05-05 18:10:35 +0300

    feat: Make bridge parser

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use reqwest::blocking::get;
+use std::error::Error;
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
+fn parse_html(url: &str) -> Result<String, Box<dyn Error>> {
+    let response = get(url)?.text()?;
+    Ok(response)
+}
+
+fn extract_bridges(text: &str) -> Vec<String> {
+    text.lines()
+        .map(str::trim)
+        .filter(|line| !line.is_empty())
+        .map(String::from)
+        .collect()
+}
+
+fn update_torrc(path: &str, bridges: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    const USE_BRIDGE: &str = "UseBridges 1";
+    const BRIDGE_BEGIN: &str = "Bridge ";
+    let input_file = File::open(path)?;
+    let reader = BufReader::new(input_file);
+    let temp_path = format!("{}.tmp", path);
+    let mut temp_file = File::create(&temp_path)?;
+    let mut use_bridges_found = false;
+    for line in reader.lines() {
+        let line = line?;
+        let trimmed = line.trim();
+        if trimmed == USE_BRIDGE {
+            if use_bridges_found {
+                eprintln!("Повтор строки '{}'", USE_BRIDGE);
+            } else {
+                use_bridges_found = true;
+                writeln!(temp_file, "{}", line)?;
+                for bridge in bridges {
+                    writeln!(temp_file, "Bridge {}", bridge)?;
+                }
+            }
+        } else if trimmed.starts_with(BRIDGE_BEGIN) {
+            continue;
+        } else {
+            writeln!(temp_file, "{}", line)?;
+        }
+    }
+    if !use_bridges_found {
+        eprintln!("Строка '{}' не найдена", USE_BRIDGE);
+    }
+    fs::rename(temp_path, path)?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let html = parse_html("https://torscan-ru.ntc.party/relays.txt")?;
+    let bridges = extract_bridges(&html);
+
+    update_torrc("torrc", &bridges)?;
+
+    Ok(())
+}
